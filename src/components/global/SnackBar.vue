09e5e603d802baa73d7289029eb2ac2709192294
srijitcoder <srijitsm123@gmail.com>
2024-10-16 13:24:07 +0530

    chores: updated snackbar code

--- src/components/global/SnackBar.vue
+++ src/components/global/SnackBar.vue
@@ -1,31 +1,19 @@
-<!-- Snackbar.vue -->
+<script setup>
+import { ref } from "vue";
+
+const visible = ref(false);
+const message = ref("");
+const color = ref("success");
+const timeout = ref(3000);
+
+const close = () => {
+  visible.value = false;
+};
+</script>
+
 <template>
   <v-snackbar v-model="visible" :timeout="timeout" :color="color">
     {{ message }}
     <v-btn color="white" text @click="close">Close</v-btn>
   </v-snackbar>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      visible: false,
-      message: "",
-      color: "success",
-      timeout: 3000,
-    };
-  },
-  methods: {
-    show({ message, color = "success", timeout = 3000 }) {
-      this.message = message;
-      this.color = color;
-      this.timeout = timeout;
-      this.visible = true;
-    },
-    close() {
-      this.visible = false;
-    },
-  },
-};
-</script>
