829361381cc54bdac39772d27a740b6bec59489b
srijitcoder <srijitsm123@gmail.com>
2024-10-23 14:46:53 +0530

    fix: build issue

--- src/api/index.js
+++ src/api/index.js
@@ -8,41 +8,64 @@ import {
   sessionReviewStatus,
 } from "@/api/session";
 import config from "@/../config.js";
+import useOctokitStore from "@/stores/octokit";
 
-const auth = await config.githubAuthToken();
-const username = config.githubOwner;
-const repo = config.githubRepo;
+export async function initOctokit() {
+  try {
+    const auth = await config.githubAuthToken();
+    const username = config.githubOwner;
+    const repo = config.githubRepo;
 
-const githubConfig = { auth, username, repo };
+    const octokit = new Octokit({ auth });
 
-const octokit = new Octokit({ auth: githubConfig.auth });
+    const { data } = await octokit.rest.users.getAuthenticated();
 
-const { data } = await octokit.rest.users.getAuthenticated();
+    return {
+      githubConfig: { auth, username, repo },
+      githubUserData: data,
+      octokit,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export async function getLoginData() {
   return data;
 }
 
 export async function getSessionsList(currPage, cache) {
-  return sessionsList(octokit, githubConfig, currPage, cache, data.login);
+  const { githubConfig, githubUserData, octokit } = useOctokitStore();
+  return sessionsList(
+    octokit,
+    githubConfig,
+    currPage,
+    cache,
+    githubUserData.login,
+  );
 }
 
 export async function deleteBySessionNumber(sessionNumber) {
+  const { githubConfig, octokit } = useOctokitStore();
   return deleteSession(octokit, githubConfig, sessionNumber);
 }
 
 export async function reviewBySessionNumber(sessionNumber, pullRequestId) {
+  const { githubConfig, octokit } = useOctokitStore();
   return reviewSession(octokit, githubConfig, sessionNumber, pullRequestId);
 }
 
 export async function getCheckStatus(sessionNumber) {
+  const { githubConfig, octokit } = useOctokitStore();
   return checkStatus(octokit, githubConfig, sessionNumber);
 }
 
 export async function getSessionReviewStatus(sessionNumber) {
+  const { githubConfig, octokit } = useOctokitStore();
   return sessionReviewStatus(octokit, githubConfig, sessionNumber);
 }
 
 export async function createSessionByName(name) {
+  const { githubConfig, octokit } = useOctokitStore();
   return createSession(octokit, githubConfig, name);
 }
