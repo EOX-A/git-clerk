48a3c680947a5605b8ac94b7441d3d69a3b8d80c
srijitcoder <srijitsm123@gmail.com>
2025-01-02 15:03:06 +0530

    chores: setup cypress with dummy test

---
+++ src/test/sessions.cy.js
@@ -0,0 +1,12 @@
+describe("Hello World Test", () => {
+  it("should display Hello World", () => {
+    cy.document().then((doc) => {
+      const div = doc.createElement("div");
+      div.innerText = "Hello World";
+      doc.body.appendChild(div);
+    });
+
+    // Check if the page contains 'Hello World'
+    cy.contains("Hello World").should("be.visible");
+  });
+});
