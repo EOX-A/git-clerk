885ed4a9a0e23120a9d7f749a0f1f7fed5a19583
srijitcoder <srijitsm123@gmail.com>
2025-02-07 17:32:21 +0530

    fix: revert env changes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,26 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    plugins: [
-      vue({
-        template: {
-          compilerOptions: {
-            isCustomElement: (tag) => tag.includes("eox-"),
-          },
+export default defineConfig({
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.includes("eox-"),
         },
-      }),
-    ],
-    server: {
-      port: 3000,
-    },
-    envPrefix: "VUE_APP_",
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+  envPrefix: "VUE_APP_",
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-  });
-};
+  },
+});
