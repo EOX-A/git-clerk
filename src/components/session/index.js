c4f33b6da3b62d1eeb5caccbd9c33d71838b8c76
srijitcoder <srijitsm123@gmail.com>
2025-02-18 14:47:30 +0530

    feat: Added session rename

--- src/components/session/index.js
+++ src/components/session/index.js
@@ -2,3 +2,4 @@ export { default as DeleteSession } from "./Delete.vue";
 export { default as ReviewSession } from "./Review.vue";
 export { default as ActionList } from "./ActionList.vue";
 export { default as GithubSession } from "./Github.vue";
+export { default as RenameSession } from "./Rename.vue";
