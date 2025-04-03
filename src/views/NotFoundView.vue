fe040b265f37351e7141d391f6577308254034d0
Silvester <silvester.pari@eox.at>
2025-04-03 15:04:11 +0200

    chore: improve not-found view (#132)

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,5 +1,12 @@
 <script setup></script>
 
 <template>
-  <h1>Not Found view</h1>
+  <div class="d-flex justify-center pa-5">
+    <v-row>
+      <v-col cols="12">
+        <h1>Error: view not found</h1>
+        <p>Seems like something went wrong. Please try restarting git-clerk.</p>
+      </v-col>
+    </v-row>
+  </div>
 </template>
