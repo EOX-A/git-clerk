92312e2516e5dba6c530e72c48ab9c846b313067
srijitcoder <srijitsm123@gmail.com>
2026-09-03 12:15:00 +0530

    chore: update eox/json form

--- src/views/FileEditView.vue
+++ src/views/FileEditView.vue
@@ -27,7 +27,6 @@ import {
   ValidationError,
 } from "@/components/file/index.js";
 import debounce from "lodash.debounce";
-// import "../../../EOxElements/elements/jsonform/dist/eox-jsonform.js";
 import "@eox/jsonform";
 import "@eox/drawtools";
 import "@eox/map";
